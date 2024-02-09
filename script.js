72ed2df11cf6957ec2dc874aa48f09262fbe67f4
MilesWK <121042782+MilesWK@users.noreply.github.com>
2024-02-09 18:07:12 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Press F12 or Right-Click the page and click inspect and then go to Console to see what this code does: 
+console.clear()
+console.log('---------------------------------------------')
+console.log('%cMilesWK', 'font-size: 24px; color: #ffffff; background-color: #000000; padding: 10px; border: 2px solid #ff0000;'); //This is styled with CSS! Did you know CSS works in the devtools section?
+console.log('%cThe developer of some basic games and animations', 'font-size: 16px;') //More CSS styling
+console.log('%cFacts about this page:\n  -All the animations are done in CSS. \n  -The only job for the JavaScript is to print things inside the console\n  -You can format text with CSS in the console. See script.js file for more.',/*More CSS:*/ 'font-size: 14px;') //Is it bad practice to put a comment inside a piece of code?
